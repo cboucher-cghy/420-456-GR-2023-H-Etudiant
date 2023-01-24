634ef1ebed2b3df24a644c6879f85d7ac29ccffe
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2023-01-24 10:58:24 -0500

    Ajout du projet initial de démonstration MVC.

---
+++ DemoLocation2000/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DemoLocation2000.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoLocation2000.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Console.WriteLine("Vive ASP.Net Core");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DemoLocation2000/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoLocation2000.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
