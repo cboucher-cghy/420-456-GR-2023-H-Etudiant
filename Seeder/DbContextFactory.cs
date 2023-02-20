8e7eb192c4797c0865a79e62b4175a1674d32ea1
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2023-02-20 13:59:31 -0500

    Ajout du Seeder
    Création du projet de Seeder avec NBuilder et Faker.

---
+++ Seeder/DbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using DemoLocation2000.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace EFCore_Seeder
+{
+    internal class DbContextFactory
+    {
+        public static ApplicationDbContext CreateDbContext()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(@Directory.GetCurrentDirectory() + "/appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("MyConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
+
+
---
+++ Seeder/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DemoLocation2000.Models;
+using EFCore_Seeder;
+using FizzWare.NBuilder;
+
+Console.WriteLine("Hello, World!");
+using var ctx = DbContextFactory.CreateDbContext();
+
+ctx.Modeles.RemoveRange(ctx.Modeles);
+ctx.SaveChanges();
+
+var listModeles = Builder<Modele>.CreateListOfSize(10)
+    .All()
+        .With(x => x.Nom = Faker.Company.Name())
+.Build();
+
+
+ctx.Modeles.AddRange(listModeles);
+ctx.SaveChanges();
+
+Console.Write("");
