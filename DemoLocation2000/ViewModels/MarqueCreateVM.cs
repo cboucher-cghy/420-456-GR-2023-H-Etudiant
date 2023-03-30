e40d68e2db9fb6ada31978f0b0dc7d561ddba69a
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2023-03-30 16:05:56 -0400

    Exemple de VM pour l'action de création

--- DemoLocation2000/ViewModels/MarqueCreateVM.cs
+++ DemoLocation2000/ViewModels/MarqueCreateVM.cs
@@ -1,7 +1,11 @@
-﻿namespace DemoLocation2000.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoLocation2000.ViewModels
 {
     public class MarqueCreateVM
     {
+        [MinLength(2)]
+        [Required(ErrorMessage = "Ce champ doit être rempli!")]
         public string Nom { get; set; } = default!;
     }
 }
