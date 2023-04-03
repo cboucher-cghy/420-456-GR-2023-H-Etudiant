84417d39c21e067293eb82ffed12f6c94dd75675
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2023-04-03 16:58:39 -0400

    Ajout d'un exemple d'utilisation du mot-clé "Yield"

---
+++ Yield-Example/YieldExample/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        IEnumerable<string> values = GetStrings().Take(10);
+        foreach (string value in values)
+        {
+            Console.WriteLine(value);
+        }
+
+        Console.ReadLine();
+    }
+
+    static IEnumerable<string> GetStrings()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            yield return GetString();
+            Console.WriteLine($"Rendu à [{i}]");
+        }
+    }
+
+    static string GetString()
+    {
+        Thread.Sleep(1000);
+        string value = new Random().Next().ToString();
+        Console.WriteLine(value);
+
+        return value;
+    }
+}
